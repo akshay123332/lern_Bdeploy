37eba8d76fa9327257c5e9777ec919b1204ee87f
akshay kumar <akshay123332@gmail.com>
2023-05-13 13:33:42 +0530

    authentication

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const mongoose=require("mongoose");
 var cors = require('cors')
 require('dotenv').config()
 const {authRouter}=require("./routes/Authentication")
-const {postRouter}=require("./routes/posts");
-const { auth } = require("./middleware/authorization");
+
+
 
 const app=express();
 app.use(express.json());
 app.use(cors())
 
 app.use("/auth",authRouter)
-app.use("/posts",auth,postRouter)
+
 
 
 app.listen(8080,async()=>{
